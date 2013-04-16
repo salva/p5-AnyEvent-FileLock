bb45ba60adbaa235479f69dd6c360232c0872fe4
Salvador <sfandino@yahoo.com>
2013-04-16 16:07:18 +0200

    add support for fcntl based locks

--- lib/AnyEvent/FileLock.pm
+++ lib/AnyEvent/FileLock.pm
@@ -13,9 +13,12 @@ use Method::WeakCallback qw(weak_method_callback);
 
 sub flock {
     my ($class, %opts) = @_;
+
+    my $type = delete $opts{type} // 'flock';
+    $type =~ /^(?:flock|fcntl)$/ or croak "invalid lock type '$type'";
+
     my $mode = delete $opts{mode} // '+<';
-    my $flock_mode = delete $opts{flock_mode} // $mode;
-    my $operation = ($flock_mode eq '<' ? Fcntl::LOCK_SH() : Fcntl::LOCK_EX());
+    my $lock_mode = delete $opts{lock_mode} // $mode;
     my $delay = delete $opts{delay} || 0.1;
 
     my $user_cb = delete $opts{cb} // croak "cb argument is missing";
@@ -36,15 +39,26 @@ sub flock {
         $fh = delete $opts{file} // croak "file or fh argument is required";
     }
 
-    %opts and croak "unkwnown arguments found (".join(', ', sort keys %opts).")";
-
     my $self = { file => $file,
                  fh => $fh,
-                 operation => $operation,
+                 type => $type,
                  max_time => $max_time,
                  user_cb => $user_cb,
                  delay => $delay };
+
     bless $self, $class;
+
+    if ($type eq 'fcntl') {
+        $self->{$_} = delete($opts{lock_start}) // 0
+            for qw(lock_start lock_whence lock_len);
+        $self->{operation} = ($lock_mode eq '<' ? Fcntl::F_RDLCK() : Fcntl::F_WRLCK());
+    }
+    else {
+        $self->{operation} = ($lock_mode eq '<' ? Fcntl::LOCK_SH() : Fcntl::LOCK_EX());
+    }
+
+
+    %opts and croak "unkwnown arguments found (".join(', ', sort keys %opts).")";
 
     my $alcb = $self->{acquire_lock_cb} = weak_method_callback($self, '_acquire_lock');
     &AE::postpone($alcb);
@@ -57,11 +71,21 @@ sub _acquire_lock {
     my $operation = $self->{opertation};
     my $now = AE::now;
 
-    if (CORE::flock($self->{fh}, $self->{operation}|Fcntl::LOCK_NB())) {
+    my $ok;
+    if ($self->{type} eq 'flock') {
+        $ok = CORE::flock($self->{fh}, $self->{operation}|Fcntl::LOCK_NB());
+    }
+    else {
+        require Fcntl::Packer;
+        my %flock = (type => $self->{operation});
+        $flock{$_} = $self->{"lock_$_"} for qw(whence start len);
+        $ok = fcntl($self->{fh}, Fcntl::F_SETLK, Fcntl::Packer::pack_fcntl_lock(\%flock));
+    }
+    if ($ok) {
         $self->{user_cb}->($self->{fh});
     }
     elsif ($! == Errno::EAGAIN() and
-         (!defined($self->{max_time}) or $self->{max_time} <= $now)) {
+           (!defined($self->{max_time}) or $self->{max_time} <= $now)) {
         &AE::timer($self->{delay}, 0, $self->{acquire_lock_cb});
         return;
     }
@@ -114,6 +138,16 @@ locking target.
 When this argument is given a file with the given name will be opened
 or created and then the module will try to lock it.
 
+=item type => $lock_type
+
+C<$lock_type> may be C<fcntl> or C<flock> and determines the way used
+to obtain the lock under the hood.
+
+The default is C<flock>.
+
+In order to use locks of type C<fcntl>, the module L<Fctnl::Packer>
+has to be also instaled.
+
 =item cb => $sub_ref
 
 The given function is called once the lock is acquired on the
@@ -123,7 +157,8 @@ In case of error (i.e. timeout) C<undef> will be passed instead of the
 file handle. The error can be retrieved from C<$!>.
 
 The user is responsible for closing the file handle or calling
-C<flock($fh, LOCK_UN)> on it when required.
+C<flock($fh, LOCK_UN)> (or the corresponding L<fcntl(2)> F<F_UNLCK>)
+on it when required.
 
 =item open_mode => $mode
 
@@ -131,7 +166,7 @@ The mode used to open the file when the argument C<file> is
 passed. Accepted values are C<< < >>, C<< > >>, C<<< >> >>>, C<< +< >>
 and C<< +> >>.
 
-=item flock_mode => $mode
+=item lock_mode => $mode
 
 The mode used when locking the file, it accepts the same set of values
 as C<open_mode>. C<< < >> means shared access and everything else
@@ -139,7 +174,7 @@ exclusive access.
 
 =item mode => $mode
 
-Configures both C<open_mode> and C<flock_mode>.
+Configures both C<open_mode> and C<lock_mode>.
 
 =item timeout => $seconds
 
@@ -153,6 +188,16 @@ failed locking attempt.
 
 Time to be delayed between consecutive locking attemps. Defaults to 1
 second.
+
+=item whence => $whence
+
+=item start => $start
+
+=item len => $len
+
+When C<fcntl> is selected as the lock type, this three additional
+arguments can be used to define the section of the file that has to be
+locked. See L<fcntl(2)> for the details.
 
 =back
 
